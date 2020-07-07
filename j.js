7c20d1148d1906ae55193a7d1ec238a885750fab
Mã  Điền <madien@Mas-MacBook-Pro.local>
2020-07-07 14:23:33 +0700

    Dasd

---
+++ j.js
@@ -0,0 +1,68 @@
+
+const exchangeRateUsdToVnd=23208;
+const exchangeRateEurToVnd=26229;
+const exchangeRateKrwToVnd=20;
+const currencyRatio = {
+  vnd: {
+    usd: 0.000043,
+    krw: 0.051,
+    eur: 0.000039,
+    vnd: 1
+  },
+  usd: {
+    usd: 1,
+    krw: 1193.27,
+    eur: 0.9,
+    vnd: 23235.5
+  },
+  krw: {
+    usd: 0.00084,
+    krw: 1,
+    eur: 0.00075,
+    vnd: 19.47
+  },
+  eur:{
+    usd:1.13,
+    krw:1350.73,
+    eur:1,
+    vnd:26240
+  }
+};
+
+
+
+
+
+function formatCurrency(type, value) {
+  const formatter = new Intl.NumberFormat(type, {
+    currency: type,
+    style: "currency"
+  });
+  return formatter.format(value);
+}
+function change(){
+  let from=document.getElementById('from').value.toLowerCase();
+  let to=document.getElementById('to').value.toLowerCase();
+  let result = amountInput.value;
+  let convertedAmount = formatCurrency(to.toUpperCase(),result * currencyRatio[from][to]);
+
+  document.getElementById("result").innerHTML = `${result} ${from.toUpperCase()} is ${convertedAmount}`;
+
+}
+
+ function reverse(){
+  let from=document.getElementById('from').value;
+  let to=document.getElementById('to').value;
+  document.getElementById('to').value=from;
+  document.getElementById('from').value=to;
+
+ }
+let amountInput = document.getElementById('amount');
+
+let convertButton= document.getElementById('convertButton');
+convertButton.addEventListener("click",change);
+let reverseButton=document.getElementById('btn-reverse');
+reverseButton.addEventListener("click",reverse);
+
+
+
